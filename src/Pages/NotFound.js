30132e571a983b8e2192abe9cc6556a538949705
Velomary Andriamasy <velomaryandria@gmail.com>
2023-10-17 14:32:05 +0300

    add style to notFound page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,8 +1,15 @@
+import "../Styles/main.css"
 const NotFound = ()=>{
     return (
+       <div className="notfound">
+
         <h1>
             Erreur 404
         </h1>
+        <p>
+            Page not found
+        </p>
+       </div>
     )
 }
 export default NotFound
