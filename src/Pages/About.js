7e2ac58a827e825540430729b36c98e80fca2227
Velomary Andriamasy <velomaryandria@gmail.com>
2023-10-17 14:11:22 +0300

    Style for about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,18 +1,17 @@
 import Navigation from "../Components/Navigation"
-
+import '../Styles/main.css'
 const About = ()=>{
     return(
-        <div>
+        <div className="about">
             <Navigation/>
             <h1 className="titre">
                 A propos de cette App:
             </h1>
-            <p>
+            <p className="about-txt">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit similique ex aliquid eveniet, amet quia commodi quos maxime odit repellendus est, hic ad tenetur quaerat dolorem tempora officia deserunt neque excepturi sapiente recusandae nemo. Quas, velit, numquam recusandae temporibus error amet quo enim modi voluptas alias cupiditate veniam eius obcaecati explicabo aperiam perferendis fugit. Dolores ullam ipsum perspiciatis aliquam cum!
             </p>
-            <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum perspiciatis soluta repudiandae tempora mollitia nostrum in rerum minima nulla perferendis. Non minus magnam necessitatibus repellat! Minima, odio sunt. Sed, a ipsa iusto dolore laudantium numquam amet accusamus atque minus nesciunt, eaque, sunt inventore fugit soluta cupiditate. Itaque beatae obcaecati sunt, odio ab quisquam explicabo voluptatibus, atque quidem doloremque eveniet provident voluptates, nam officiis asperiores. Ad nesciunt reiciendis unde ut amet quisquam debitis impedit aliquam adipisci sed exercitationem, culpa voluptatem nobis aperiam! Consectetur, dolorem aspernatur? Sint, adipisci. Non, eum hic molestiae necessitatibus reiciendis debitis ut, placeat nesciunt doloremque eligendi asperiores ab magni. Maiores quia ratione porro quas deleniti pariatur animi! Aut non molestiae nihil nemo qui. Nostrum at explicabo illo animi officia dignissimos facere quas! Delectus, totam maiores? Inventore totam consequuntur iste! Voluptas, alias repellendus? Quasi laborum quos rerum placeat dicta!
-            </p>
+            <p className="about-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias maxime debitis veritatis quibusdam ipsam neque minus placeat, impedit accusantium magnam dolor fuga beatae quia corporis consectetur quam fugit perferendis sint error vero? Recusandae numquam reprehenderit consectetur labore sunt, vitae aut quidem, explicabo nisi soluta dolore magnam cupiditate porro autem dolorem nemo qui? Totam cumque eligendi natus harum doloremque minima nostrum est hic dolores nulla, pariatur doloribus repellendus suscipit eum sint mollitia, distinctio laborum provident voluptatum nesciunt? Quibusdam aliquam reprehenderit, quis alias doloremque error aperiam totam quisquam eos placeat molestias impedit. Laboriosam eum, soluta esse corrupti temporibus consequatur porro voluptate in cum aliquid doloremque recusandae, at asperiores corporis quas numquam provident accusantium facere necessitatibus ratione reiciendis natus voluptas suscipit. Quas, sint.</p>
+            
         </div>
     )
 }
